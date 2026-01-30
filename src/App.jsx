9c252e1213c1820b176f82f8f7b479099cb8970c
Danielasan14 <barrerodaniela2@gmail.com>
2026-01-29 19:10:33 -0500

    add-counter-with-negative-number

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [discount, setDiscount] = useState(0)
 
   return (
     <>
@@ -22,10 +23,10 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
+        <button onClick={() => setDiscount((discount) => discount - 1)}>
+          count is {discount}
+        </button>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
